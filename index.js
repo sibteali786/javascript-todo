7201e8b480174cd7a9101c6c19858fe624e66ac7
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-10-08 20:55:42 +0500

    implemented Get items

--- index.js
+++ index.js
@@ -2,37 +2,14 @@ const listContent = document.querySelector(".content");
 const itemName = document.querySelector("#item-name");
 const addButton = document.querySelector(".add-btn");
 
-console.log(listContent);
-console.log(itemName);
-console.log(addButton);
-
-// Empty Array item
-var item = [];
-
-// getting localStorage data into localStorageItems
-const localStorageItems = localStorage.getItem("items");
-
-// condition to assign empty item array values if localStorage items is not already empty
-if (localStorageItems !== null) {
-  // has to do items
-  item = JSON.parse(localStorageItems);
-}
-console.log(localStorageItems);
-addButton.addEventListener("click", (e) => {
-  const itemValue = itemName.value;
-  item.push(itemName.value);
-
-  const JSONItems = JSON.stringify(item);
-  localStorage.setItem("items", JSONItems);
-
-  itemName.value = ""; // so that after we write and add something previous value disappears
+const addItems = (toDoItem) => {
   // List Item
   const liItem = document.createElement("li");
   liItem.className = "item";
 
   // p tag
   const pItem = document.createElement("p");
-  const pText = document.createTextNode(itemValue); // For putting text in something
+  const pText = document.createTextNode(toDoItem); // For putting text in something
 
   // Adding text in p tag
   pItem.appendChild(pText);
@@ -52,6 +29,37 @@ addButton.addEventListener("click", (e) => {
   deleteBtn.addEventListener("click", (e) => {
     e.target.parentElement.remove();
   });
+};
+
+// Empty Array item
+var item = [];
+var id = 1;
+// getting localStorage data into localStorageItems
+const localStorageItems = localStorage.getItem("items");
+
+// condition to assign empty item array values if localStorage items is not already empty
+if (localStorageItems !== null) {
+  // has to do items
+  item = JSON.parse(localStorageItems);
+  for (var i = 0; i < item.length; i++) {
+    const currentItem = item[i];
+    addItems(currentItem.name);
+  }
+}
+console.log(localStorageItems);
+addButton.addEventListener("click", (e) => {
+  const itemValue = itemName.value;
+  const newItem = {
+    id: new Date().getTime(),
+    name: itemValue,
+  };
+  item.push(newItem);
+
+  const JSONItems = JSON.stringify(item);
+  localStorage.setItem("items", JSONItems);
+
+  itemName.value = ""; // so that after we write and add something previous value disappears
+  addItems(itemValue);
 });
 
 // How to save something in browser as a local value without any data base
